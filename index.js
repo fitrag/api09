5a36573b1c38f53fa4ef64f51c3fdd4837d37feb
fitrag <34418986+fitrag@users.noreply.github.com>
2021-03-06 10:03:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const app = express()
 
 const berita = require('./berita')
 const insidelearn = require('./learn.js')
+const resep = require('./resep')
 
 app.use(cors())
 
 app.use("/api", berita)
 app.use("/api", insidelearn)
+app.use("/api", resep)
 
 app.listen(PORT, () => {
     console.log("Server running " + PORT)
